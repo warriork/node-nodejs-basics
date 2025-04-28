2092d2f39a3628172d8120c0417a50e35791d894
warriork <warriork123456@gmail.com>
2025-04-29 00:54:36 +0200

    subtask copy js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    const sourceFolder = path.join(__dirname, 'files');
+    const targetFolder = path.join(__dirname, 'files_copy');
+
+    if (fs.existsSync(targetFolder) || !fs.existsSync(sourceFolder)) {
+        throw new Error(`FS operation failed`);
+    }
+    try {
+        await fsPromises.mkdir(targetFolder);
+        const files = await fsPromises.readdir(sourceFolder);
+
+        await Promise.all(
+            files.map(file => {
+                const sourcePath = path.join(sourceFolder, file);
+                const targetPath = path.join(targetFolder, file);
+                return fsPromises.copyFile(sourcePath, targetPath);
+            })
+        );
+
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await copy();
