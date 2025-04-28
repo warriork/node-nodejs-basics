23a64a25791415670466d8eb10e1df183ad15f89
warriork <warriork123456@gmail.com>
2025-04-29 00:38:47 +0200

    subtask create js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import { promises as fsPromises } from 'fs';
+import path from 'path';
 
+const create = async () => {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    const targetFile = path.join(__dirname, 'files', 'fresh.txt')
+
+    if (fs.existsSync(targetFile)) {
+        throw new Error(`FS operation failed`);
+    } else {
+        const content =  'I am fresh and young'
+        try {
+           await fsPromises.writeFile(targetFile, content, { encoding: 'utf8' })
+        } catch (err) {
+            console.error(err)
+        }
+    }};
 await create();
