f7fd6d2ecbc2eca920d5849d86f8118916aa595f
warriork <warriork123456@gmail.com>
2025-04-29 01:00:47 +0200

    file system tasks solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemovePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+    if (!fs.existsSync(fileToRemovePath)) {
+        throw new Error(`FS operation failed`);
+    }
+    try {
+        await fs.promises.unlink(fileToRemovePath);
+        console.log(`file ${fileToRemovePath} removed`);
+    } catch (e) {
+        console.err(e)
+    }
+
+
 };
 
 await remove();
