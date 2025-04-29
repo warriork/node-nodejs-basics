4bcd84805c718a7af64d2567493ff22114834c8a
warriork <warriork123456@gmail.com>
2025-04-29 23:37:33 +0200

    streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+ const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+ if (!fs.existsSync(filePath)) {throw new Error ('FS operation failed')}
+ const stream = fs.createReadStream(filePath);
+
+ stream.pipe(process.stdout);
 };
 
 await read();
