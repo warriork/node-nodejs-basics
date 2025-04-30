8786cceeca9f58f4f18e910229a80b42a48a0ae1
warriork <warriork123456@gmail.com>
2025-04-30 22:02:39 +0200

    zip task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import path from 'path'
+import zlib from 'zlib'
+import { pipeline } from 'stream/promises';
+import * as fs from "node:fs";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const transform = async () => {
-    // Write your code here 
+
+    const inputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const outputPath = path.join(__dirname, 'files', 'archive.gz');
+    const readStream = fs.createReadStream(inputPath);
+    const writeStream = fs.createWriteStream(outputPath);
+    const gZipStream = zlib.createGzip()
+
+    try {
+        await pipeline(readStream, gZipStream, writeStream);
+        console.log('file was zipped successfully.');
+    } catch (error) {console.error(error);}
+
 };
 
 await transform();
